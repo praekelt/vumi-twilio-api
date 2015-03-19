71289d3a22476001421454ff736ea03742e43158
Rudi Giesler <rudi@praekelt.com>
2015-03-19 15:24:45 +0200

    Add basic parser

--- vumi_twilio_api/twilml_parser.py
+++ vumi_twilio_api/twilml_parser.py
@@ -1,3 +1,6 @@
+import xml.etree.ElementTree as ET
+
+
 class Verb(object):
     """Represents a single verb in TwilML. """
 
@@ -5,3 +8,27 @@ class Verb(object):
         self.verb = verb
         self.attributes = attributes
         self.nouns = nouns
+
+
+class TwilMLParseError(Exception):
+    """Raised when trying to parse invalid TwilML"""
+
+
+class TwilMLParser(object):
+    """Parser for TwilML"""
+
+    def parse_xml(self, xml):
+        """Parses TwilML and returns a list of :class:`Verb` objects"""
+        verbs = []
+        root = ET.fromstring(xml)
+        if root.tag != "Response":
+            raise TwilMLParseError(
+                "Invalid root %r. Should be 'Request'." % root.tag)
+        for child in root:
+            parser = getattr(
+                self, '_parse_%s' % child.tag, self._parse_default)
+            verbs.append(parser(child))
+        return verbs
+
+    def _parse_default(self, element):
+        raise TwilMLParseError("Unable to parse verb %r" % element.tag)
