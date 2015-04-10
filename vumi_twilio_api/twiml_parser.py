228fce71faafdad73103b983f8a11c6dc2f88a91
Rudi Giesler <rudi@praekelt.com>
2015-04-10 15:30:51 +0200

    Add parsing for Gather verb

--- vumi_twilio_api/twiml_parser.py
+++ vumi_twilio_api/twiml_parser.py
@@ -1,4 +1,5 @@
 import re
+from urlparse import urljoin
 import xml.etree.ElementTree as ET
 
 
@@ -51,6 +52,77 @@ class Hangup(Verb):
         return cls()
 
 
+class Gather(Verb):
+    """Represents the Gather verb"""
+    name = "Gather"
+
+    @classmethod
+    def from_xml(cls, xml, url):
+        """Returns a new Gather verb from the given ElementTree object.
+        The URL is the document url, used as the default action URL."""
+        action = xml.attrib.get('action', None)
+        action = urljoin(url, action)
+
+        valid_methods = ['GET', 'POST']
+        method = xml.attrib.get('method', 'POST')
+        if method not in valid_methods:
+            raise TwiMLParseError(
+                "Invalid value %r for method attribute. Must be one of %r" % (
+                    method, valid_methods))
+
+        timeout = xml.attrib.get('timeout', 5)
+        try:
+            timeout = int(timeout)
+        except ValueError:
+            raise TwiMLParseError(
+                "Invalid value %r for timeout parameter. Must be an integer."
+                % timeout)
+        if timeout < 0:
+            raise TwiMLParseError(
+                "Invalid value %r for timeout parameter. Must be positive"
+                % timeout)
+
+        finishOnKey = xml.attrib.get('finishOnKey', '#')
+        if len(finishOnKey) > 1:
+            raise TwiMLParseError(
+                "Invalid value %r for finishOnKey parameter. "
+                "Must only be one character" % finishOnKey)
+        valid_chars = "0123456789#*"
+        if finishOnKey not in valid_chars:
+            raise TwiMLParseError(
+                "Invalid value %r for finishOnKey parameter. "
+                "Must be one of %r" % (finishOnKey, valid_chars))
+
+        numDigits = xml.attrib.get('numDigits', None)
+        if numDigits is not None:
+            try:
+                numDigits = int(numDigits)
+            except ValueError:
+                raise TwiMLParseError(
+                    "Invalid value %r for numDigits parameter. "
+                    "Must be an integer." % numDigits)
+            if numDigits < 1:
+                raise TwiMLParseError(
+                    "Invalid value %r for numDigits parameter. Must be >=1"
+                    % numDigits)
+
+        data = TwiMLParser.from_list(xml)
+        valid_verbs = ['Say', 'Play']
+        for verb in data:
+            if verb.name not in valid_verbs:
+                raise TwiMLParseError(
+                    "Invalid sub verb %r for Gather verb. Must be one of %r"
+                    % (verb.name, valid_verbs))
+
+        return cls({
+            'action': action,
+            'method': method,
+            'timeout': timeout,
+            'finishOnKey': finishOnKey,
+            'numDigits': numDigits,
+            }, data)
+
+
 class TwiMLParseError(Exception):
     """Raised when trying to parse invalid TwilML"""
 
@@ -66,6 +138,16 @@ class TwiMLParser(object):
             raise TwiMLParseError(
                 "Invalid root %r. Should be 'Request'." % root.tag)
         for child in root:
+            parser = getattr(
+                self, '_parse_%s' % child.tag.lower(), self._parse_default)
+            verbs.append(parser(child))
+        return verbs
+
+    @classmethod
+    def from_list(cls, lst):
+        self = cls()
+        verbs = []
+        for child in lst:
             parser = getattr(
                 self, '_parse_%s' % child.tag.lower(), self._parse_default)
             verbs.append(parser(child))
