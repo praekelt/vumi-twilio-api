a783c5d6457fd265d03e8920baf9bce04af0d861
Rudi Giesler <rudi@praekelt.com>
2015-04-14 17:07:51 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vxtwinio",
-    version="0.0.1",
+    version="0.0.2a",
     url="https://github.com/praekelt/vumi-twilio-api",
     license="BSD",
     description="Provides a REST API to Vumi that emulates the Twilio API",
